491f57b0af4940a710ce56fd8f2f3698393c79cc
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:02:40 +0530

    Add project files.

---
+++ LamdaExpressionDemo/LamdaExpresClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressionDemo
+{
+    //define delegate
+    public delegate string GreetingDelegate(string str);
+    internal class LamdaExpresClass
+    {
+        public static string Greetings(string name)
+        {
+            return "Hello " + name + ", Welcome!";
+        }
+        static void Main()
+        {
+            //instantiation of delegate
+            GreetingDelegate dg = new GreetingDelegate(Greetings);
+
+            string s = dg.Invoke("Neha");
+            Console.WriteLine(s);
+
+            Console.WriteLine("\n\nLamda Expression :\n");
+            //lamda expression
+            GreetingDelegate dg1 = (name) =>
+            {
+                return "Hello " + name + ", Welcome!";
+            };
+            string s1 = dg1.Invoke("Pranita");
+            Console.WriteLine(s1);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
